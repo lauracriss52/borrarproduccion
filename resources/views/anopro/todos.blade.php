d766b86977eef81c45623a6123473d879e1bafbf
Yesid Alejandro Sanchez Diaz <yesid.alejo300@gmail.com>
2016-08-11 17:39:53 -0500

    Add files via upload

---
+++ resources/views/anopro/todos.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+@section('contenido')
+	@if(sizeof($fincas) > 0)
+
+		<table class="table table-striped table-hover">
+			<thead>
+				<tr>
+					<th>Id</th>
+					<th>Id_sie_ano </th>
+                    <th>Ano_ano </th>
+					<th>Proreal_ano </th>
+					<th>Proesperda_ano </th>
+					
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($fincas as $fincas)
+				<tr>
+					<td>{{$anopro->id}}</td>
+                    <td>{{$anopro->Id_sie_ano}}</td>
+                    <td>{{$anopro->Ano_ano}}</td>
+                    <td>{{$anopro->Proreal_ano}</td>
+                    <td>{{$anopro->Proesperda_ano}}</td>
+					
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+		@else
+
+		<div class="alert alert-danger">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			<strong>Ups</strong> No hay produccion este año  
+		</div>
+
+		@endif
+@endsection

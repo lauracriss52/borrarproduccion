92230a0ee89e9f2f071431f4778c318adc77b188
Esneider Alonso Ortiz Vargas <eaortiz328@misena.edu.co>
2016-08-11 17:52:47 -0500

    Add files via upload
    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace produccion\Http\Controllers;
-
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -22,7 +20,6 @@ class Controller extends BaseController
 		//se crea una instancia de client para el Guzzlehttp, para realizar peticiones a sitios seguros https es necesrio un certificado de seridad para ello, y se le indica al cliente donde esta ubicado para que cada vez que solicite una conexión segura y el equipo pida un certificado utilice este.
 		//base_parh es una función de laravel para buscar el directorio raiz
     	$cliente = new Client(['curl' => [CURLOPT_CAINFO => base_path('resources/certs/cacert.pem')]]);
-
 		//Request resive los parametos de la línea 20
     	$respuesta = $cliente->request($metodo, $url, $parametros);
     	return $respuesta->getBody()->getContents();
@@ -35,7 +32,7 @@ class Controller extends BaseController
     	$clientId = config('api.client_id');
     	$clientSecret = config('api.client_secret');
     	$grantType = config('api.grant_type');
-    	$respuesta = json_decode($this->realizarPeticion('POST', 'http://agroproduccion.com:8889/oauth/access_token', ['form_params' => ['grant_type' => $grantType, 'client_id' => $clientId, 'client_secret' => $clientSecret]]));
+    	$respuesta = json_decode($this->realizarPeticion('POST', 'http://agroproduccion.com:81/oauth/access_token', ['form_params' => ['grant_type' => $grantType, 'client_id' => $clientId, 'client_secret' => $clientSecret]]));
     	$accessToken = $respuesta->access_token;
 		//echo $accessToken;
     	return $accessToken;

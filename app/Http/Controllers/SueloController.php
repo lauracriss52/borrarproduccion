043dc14b8d4d07d9ef9edaea86cfa8d3d5c58e85
Yesid Alejandro Sanchez Diaz <yesid.alejo300@gmail.com>
2016-09-07 16:09:43 -0500

    Add files via upload

--- app/Http/Controllers/SueloController.php
+++ app/Http/Controllers/SueloController.php
@@ -10,11 +10,75 @@ use produccion\Http\Requests\UnicoRequest;
 
 class SueloController extends ClienteController
 {
-    //método para mostrar las Productos
+    //método para mostrar las suelo
 	
-	public function mostrarSuelo()
+	public function mostrarSuelos()
 	{
-		$suelo = $this->ListarTodosLosSuelos();
+		$suelo = $this->obtenerTodosLosSuelos();
 		return view('suelo.todos', ['suelo' => $suelo]);
 	}
+	
+	public function obtenerSuelo(UnicoRequest $request)
+    {
+        $Id_sue = $request->get('Id_sue');
+        $suelo = $this->obtenerUnSuelo($Id_sue);
+        return view('suelo.mostrar', ['suelo' => $suelo]);
+    }
+
+	public function mostrarSuelo()
+    {
+        return view('suelo.unico');
+    }
+    
+	//métodos para agregar suelo
+    public function agregarSuelo()
+    {
+        return view('suelo.agregar');
+    }
+	
+ 
+ 
+    public function crearSuelo(Request $request)
+    {
+
+        $this->almacenarSuelo($request);
+        return redirect('/suelo');
+    }
+	
+		
+	//Métodos para Modificar suelo
+    //1. 
+    public function elegirSuelo()
+    {
+        $suelo = $this->obtenerTodosLosSuelos();
+        return view('suelo.elegir', ['suelo' => $suelo]);
+    }
+	
+	public function editarSuelo(Request $request)
+    {
+        $Id_sue = $request->get('suelo_	Id_sue');
+        $suelo = $this->obtenerUnSuelo($Id_sue);
+        return view('suelo.editar', ['suelo' => $suelo]);
+    }
+	
+	
+    public function actualizarSuelo(Request $request)
+    {
+        $this->modificarSuelo($request);
+        return redirect('/suelo');
+    }
+	
+	//Método para Eliminar suelo
+	
+		    public function seleccionarSuelo()
+    {
+        $suelo = $this->obtenerTodosLosSuelos();
+        return view('suelo.seleccionar', ['suelo' => $suelo]);
+    }
+	
+    public function eliminarSuelo(Request $request)
+    {
+        $this->removerSuelo($request);
+        return redirect('/suelo');
+    }
 }
